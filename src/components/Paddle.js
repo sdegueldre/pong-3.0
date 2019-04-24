37ffabfe900d8268dcd797b00e4e3b1a8b236a4f
NadTr <trauflernadine@gmail.com>
2019-04-24 16:48:14 +0200

    get base pong working

--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -11,13 +11,27 @@ export default class Paddle extends PIXI.Graphics {
       color: 0xFFFFFF
     }
     Object.assign(defaults, options);
-    for(let key in defaults){
-      this[key] = defaults[key];
-    }
+    Object.assign(this, defaults);
+
     console.log(defaults, options);
 
     this.beginFill(this.color);
     this.drawRect(-this.w/2, -this.h/2, this.w, this.h);
     this.endFill();
+  }
+  collide(ball){
+    var distX = Math.abs(ball.x - this.x)-this.w/2-ball.radius;
+    var distY = Math.abs(ball.y - this.y)-this.h/2-ball.radius;;
+
+    if (distX > 0) { return false; }
+    if (distY > 0) { return false; }
+
+    if (distX <= 0) { return true; }
+    if (distY <= 0) { return true; }
+
+    var dx=distX-this.w/2;
+    var dy=distY-this.h/2;
+    return (dx*dx+dy*dy<=(ball.r*ball.r));
+    // return ((ball.x - ball.radius < this.x) || (ball.x + ball.radius >this.x));
   }
 }
--- src/pong3.js
+++ src/pong3.js
@@ -1,47 +1,44 @@
 import * as PIXI from 'pixi.js';
 import Paddle from './components/Paddle';
 
-
+import Field from './components/Field';
+import Ball from './components/Ball';
 
 const app = new PIXI.Application({ antialias: true });
 document.body.appendChild(app.view);
 
-const graphics = new PIXI.Graphics();
-console.log(app.view);
-// document.addEventListener('click', () => app.view.requestFullscreen());
-
-// Rectangle
-// graphics.beginFill(0xFFFFFF);
-// graphics.drawRect(20 , ((app.screen.height)/2)-50, 20, 100);
-// graphics.endFill();
-// Rectangle2
-graphics.beginFill(0xFFFFFF);
-graphics.drawRect(app.screen.width-40, app.screen.height/2 -50, 20, 100);
-graphics.endFill();
-
-//Filet
-for (let i=0; i*50<app.screen.height ;i++){
-  graphics.beginFill(0xFFFFFF);
-	graphics.drawRect(app.screen.width/2 -2.5, 50*i+10, 5, 30);
-  graphics.endFill();
-}
-
-// Ball
-graphics.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
-graphics.beginFill(0xF91818, 1);
-graphics.drawCircle(250, 250, 10);
-graphics.endFill();
-graphics.interactive = true;
-
-graphics.on('mousemove', (e) => {
-  graphics.x += e.data.originalEvent.movementX;
-  graphics.y += e.data.originalEvent.movementY;
-});
-
 window.addEventListener('keydown', console.log);
 
 // app.stage.addChild(graphics);
-let player1 = new Paddle({x: 30, y: app.screen.height/2});
-let player2 = new Paddle({x: app.screen.width-30, y: app.screen.height/2});
-console.log(player1);
+let field = new Field({
+  h: app.screen.height,
+  w: app.screen.width
+});
+app.stage.addChild(field);
+
+let player1 = new Paddle({
+  x: 30,
+  y: app.screen.height/2
+});
+player1.interactive = true;
+player1.on('mousemove', (e) => {
+  player1.y = e.data.global.y;
+});
+let player2 = new Paddle({
+  x: app.screen.width-30,
+  y: app.screen.height/2
+});
+player2.interactive = true;
+player2.on('mousemove', (e) => {
+  player2.y = e.data.global.y;
+});
 app.stage.addChild(player1, player2);
+
+let ball = new Ball({
+  ticker: app.ticker,
+  field:field,
+  x:300,
+  y:150,
+  paddles:[player1,player2],
+});
+app.stage.addChild(ball);
