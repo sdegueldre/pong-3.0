f1679f98554beccacd356fe6d142e7f20fe05577
Danil Kolumbet <kolumbetko@gmail.com>
2021-02-20 22:14:37 +0200

    [IMP] add limits to user and room names

--- src/client/components/NameSelector.jsx
+++ src/client/components/NameSelector.jsx
@@ -5,13 +5,13 @@ const NameSelector = ({chooseUserName}) => {
     ev.preventDefault();
     const formData = new FormData(ev.target);
     const {userName} = Object.fromEntries(formData.entries());
-    chooseUserName(userName);
+    chooseUserName(userName.trim());
   };
 
   return (
     <div className="username-selector">
       <form className="d-flex" onSubmit={userNameSubmit} style={{fontSize: '1rem'}}>
-        <input placeholder="Enter a nickname..." name="userName" className="neon-border" required />
+        <input maxLength="25" placeholder="Enter a nickname..." name="userName" className="neon-border" required pattern=".*\S+.*" title="Come up with a nickname for yourself" />
         <button type="submit" className="ml">Go</button>
       </form>
     </div>
